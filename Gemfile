4b97cd247182c55d8be5a815c64de1d6e84bd348
Vodka7 <dainis186@gmail.com>
2012-09-28 14:13:57 +0300

    add ...

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :assets do
 
   gem 'uglifier', '>= 1.0.3'
 end
+gem 'capistrano'
 gem 'paperclip', '~> 3.0'
 gem 'rack-mini-profiler'
 gem 'jquery-rails'
---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+working_directory "/home/ubuntu/www/QuestionsAnswers"
+pid "/home/ubuntu/www/QuestionsAnswers/tmp/pids/unicorn.pid"
+stderr_path "/home/ubuntu/www/QuestionsAnswers/unicorn/err.log"
+stdout_path "/home/ubuntu/www/QuestionsAnswers/unicorn/out.log"
+
+listen "/tmp/unicorn.todo.sock"
+preload_app true
+worker_processes 2
+timeout 30
